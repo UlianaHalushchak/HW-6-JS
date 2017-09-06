b4730780686a5967c08c3ab0a7f8f7d2f0ed3884
Uliana Halushchak <30951313+UlianaHalushchak@users.noreply.github.com>
2017-09-06 22:56:13 +0300

    Update script6.js

--- script6.js
+++ script6.js
@@ -1,18 +1,18 @@
-var euro = document.getElementById("txt");
-	var dollar = document.getElementByClass("myList2");
-	var euroDollar = document.getElementByClass("myList3");
+var euro = document.getElementById("list1");
+	var dollar = document.getElementById("list2");
+	var euroDollar = document.getElementById("list3");
 
 function myFunction1() {
-    var euro = prompt("How much whould you like to change?")
-    alert("You will have " + euro * 30 + " UAH");
+    var euro = prompt("change euro")
+    alert("your money" + euro * 30 + " UAH");
 }
 
 function myFunction2() {
-    var dollar = prompt("How much whould you like to change?")
-    alert("You will have " + dollar * 25 + "UAH");
+    var dollar = prompt("change dollar")
+    alert("your money " + dollar * 25 + "UAH");
 }
 
 function myFunction3() {
-    var euroDollar = prompt("How much whould you like to change?")
-    alert("You will have " + euroDollar * 1.08 + "$");
+    var euroDollar = prompt("change?")
+    alert("your money " + euroDollar * 1.08 + "$");
 }
