4357f344a542f010dbce0d05713705b749d3c85c
Uliana Halushchak <30951313+UlianaHalushchak@users.noreply.github.com>
2017-09-06 23:29:40 +0300

    Update script6.js

--- script6.js
+++ script6.js
@@ -2,17 +2,17 @@ var euro = document.getElementById("list1");
 	var dollar = document.getElementById("list2");
 	var euroDollar = document.getElementById("list3");
 
-function myFunction1() {
+function change1() {
     var euro = prompt("change euro")
     alert("your money" + euro * 30 + " UAH");
 }
 
-function myFunction2() {
+function change2() {
     var dollar = prompt("change dollar")
     alert("your money " + dollar * 26 + "UAH");
 }
 
-function myFunction3() {
+function change3() {
     var euroDollar = prompt("change euro per dollar")
     alert("your money " + euroDollar * 1.08 + "$");
 }
